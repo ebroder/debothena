2ed45b10518e82a85a2c19835e4e4f9726365f54
Evan Broder <broder@mit.edu>
2010-04-26 16:01:36 -0400

    Be more liberal in finding Trac references.

--- debothena.py
+++ debothena.py
@@ -24,7 +24,7 @@ def build_matcher(regex, flags=0):
     return match
 
 matchers = (
-    ('Debathena', [build_matcher(r'\btrac[- ]#([0-9]+)\b', re.I)]),
+    ('Debathena', [build_matcher(r'\btrac[-\s:]*#([0-9]+)\b', re.I)]),
 
     ('Debathena', [build_matcher(r'#([0-9]+)\b')])
     )
